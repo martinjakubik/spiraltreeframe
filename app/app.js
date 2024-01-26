127ad797e2d4d2c38fd3357f98378e0ebcf9c984
martin jakubik <martin.jakubik@gmail.com>
2024-01-26 23:48:27 +0100

    draws but it's wrong

--- app/app.js
+++ app/app.js
@@ -1,4 +1,19 @@
 import Monifesto from "./monifesto.mjs";
 
 const monifesto = new Monifesto();
-monifesto.line({x: 80, y:400, width:440, height:0});
+
+const aPoints = [ {x: 20, y: 0}, {x: 10, y: 30}, {x: 8, y: 38}, {x: 6, y: 22}];
+
+const oStartPoint = {x: 5, y: 0};
+
+aPoints.forEach((oPoint, nIndex, aPointArray) => {
+    let oPreviousPoint = oStartPoint;
+    if (nIndex > 0) {
+        oPreviousPoint = aPointArray[nIndex - 1];
+    }
+    const x = oPreviousPoint.x;
+    const y = oPreviousPoint.y;
+    const width = oPoint.x - oPreviousPoint.x;
+    const height = oPoint.y - oPreviousPoint.y;
+    monifesto.line({x: x, y: y, width: width, height: height});
+});
